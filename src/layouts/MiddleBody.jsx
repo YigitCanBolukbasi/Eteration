69151b5a7e4e5065e9c1500f670ce822fa7a70a4
Yiğit Can bölükbaşı <91588692+YigitCanBolukbasi@users.noreply.github.com>
2023-04-15 21:45:13 +0300

    pagination

--- src/layouts/MiddleBody.jsx
+++ src/layouts/MiddleBody.jsx
@@ -1,22 +1,35 @@
 import React, { useEffect, useState } from "react";
 import CardX from "../components/Card/CardX";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Pagination } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductsAsync } from "../redux/products/productsSlice";
 
 function MiddleBody({ cartItems, setCartItems }) {
   const dispatch = useDispatch();
+  const itemsPerPage = 12; // Her sayfada gösterilecek ürün sayısı
+  const [currentPage, setCurrentPage] = useState(1);
   const items = useSelector((state) => state.products.filteredItems);
 
   useEffect(() => {
     dispatch(getProductsAsync());
   }, [dispatch]);
 
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
+
+  // Sayfa başına öğeleri filtrelemek için bir fonksiyon oluşturun
+  const filterItemsPerPage = (items, currentPage) => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return items.slice(startIndex, endIndex);
+  };
+
   return (
     <Box sx={{ width: "100%" }}>
       <Grid container rowSpacing={5} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <CardX
-          products={items}
+          products={filterItemsPerPage(items, currentPage)}
           cartItems={cartItems}
           setCartItems={setCartItems}
           onAddCardClick={(product) => {
@@ -24,6 +37,12 @@ function MiddleBody({ cartItems, setCartItems }) {
           }}
         />
       </Grid>
+      <Pagination
+        count={Math.ceil(items.length / itemsPerPage)}
+        page={currentPage}
+        onChange={handlePageChange}
+        sx={{ display: "flex", justifyContent: "center", marginTop: 5 }}
+      />
     </Box>
   );
 }
